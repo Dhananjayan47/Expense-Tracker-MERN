cd171fd10efeba99409ee02221be6d8dcc6cef47
Dhananjayan47 <kdhanush181818@gmail.com>
2025-11-07 12:13:59 +0530

    updated

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -13,8 +13,8 @@ const expenseRoutes=require('./routes/expenseRouters');
 dotenv.config();
 
 connectDB()
-app.use(cookieParser());
 const app=express();
+app.use(cookieParser());
 app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials: true,
